cb94ab6a2315517b98f629905b4ce5c9719b2959
fastjonh <fastjonh@gmail.com>
2023-09-12 06:23:18 -0300

    making operators return result

--- Rust/math_parser/src/interpreter.rs
+++ Rust/math_parser/src/interpreter.rs
@@ -3,24 +3,49 @@ use rust_decimal::Decimal;
 use crate::parser::{Expression, Operator};
 
 pub fn evaluate(expression: Expression) -> Result<Decimal, SemanticError> {
-    Ok(expression.evaluate())
+    expression.evaluate()
 }
 
 impl Expression {
-    pub fn evaluate(&self) -> Decimal {
+    pub fn evaluate(&self) -> Result<Decimal, SemanticError> {
         match self {
-            Expression::Number(n) => *n,
+            Expression::Number(n) => Ok(*n),
             Expression::Binary { lhs, rhs, op } => binary(lhs, rhs, op),
+        }
+    }
+
+    pub fn add(&self, rhs: &Expression) -> Result<Decimal, SemanticError> {
+        Ok(self.evaluate()? + rhs.evaluate()?)
+    }
+
+    pub fn sub(&self, rhs: &Expression) -> Result<Decimal, SemanticError> {
+        Ok(self.evaluate()? - rhs.evaluate()?)
+    }
+
+    pub fn mult(&self, rhs: &Expression) -> Result<Decimal, SemanticError> {
+        Ok(self.evaluate()? * rhs.evaluate()?)
+    }
+
+    pub fn div(&self, rhs: &Expression) -> Result<Decimal, SemanticError> {
+        let rhs_v = rhs.evaluate()?;
+
+        if rhs_v.is_zero() {
+            Err(SemanticError::DivisionByZero {
+                lhs: self.to_owned(),
+                rhs: rhs.to_owned(),
+            })
+        } else {
+            Ok(self.evaluate()? / rhs_v)
         }
     }
 }
 
-fn binary(lhs: &Expression, rhs: &Expression, op: &Operator) -> Decimal {
+fn binary(lhs: &Expression, rhs: &Expression, op: &Operator) -> Result<Decimal, SemanticError> {
     match op {
-        Operator::Add => lhs.evaluate() + rhs.evaluate(),
-        Operator::Sub => lhs.evaluate() - rhs.evaluate(),
-        Operator::Div => lhs.evaluate() / rhs.evaluate(),
-        Operator::Mult => lhs.evaluate() * rhs.evaluate(),
+        Operator::Add => lhs.add(rhs),
+        Operator::Sub => lhs.sub(rhs),
+        Operator::Div => lhs.div(rhs),
+        Operator::Mult => lhs.mult(rhs),
     }
 }
 
