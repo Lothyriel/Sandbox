8f7dd1518654505e6772a0e4652298055672f3b9
fastjonh <fastjonh@gmail.com>
2023-09-13 01:21:06 -0300

    adding tests

--- Rust/math_rs/src/lib.rs
+++ Rust/math_rs/src/lib.rs
@@ -21,3 +21,20 @@ pub enum MathError {
     #[error("{0}")]
     Semantic(#[from] interpreter::SemanticError),
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_assert_simple_expression() -> Result<(), MathError> {
+        assert_eq!(evaluate("4+5")?, 9.into());
+        Ok(())
+    }
+
+    #[test]
+    fn should_assert_long_expression() -> Result<(), MathError> {
+        assert_eq!(evaluate("1+2+3+4+5")?, 15.into());
+        Ok(())
+    }
+}
