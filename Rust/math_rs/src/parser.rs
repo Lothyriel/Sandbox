4ce16611fabf80fdae90085a9e5b7202421d8b9f
fastjonh <fastjonh@gmail.com>
2023-09-13 01:06:05 -0300

    moving to struct with impl block

--- Rust/math_rs/src/parser.rs
+++ Rust/math_rs/src/parser.rs
@@ -4,87 +4,97 @@ use rust_decimal::Decimal;
 
 use crate::lexer::Token;
 
-pub fn parse(mut tokens: VecDeque<Token>) -> Result<Expression, SyntacticError> {
-    get_expression(&mut tokens)
+pub fn parse(tokens: VecDeque<Token>) -> Result<Expression, SyntacticError> {
+    Parser::new(tokens).get_expression()
 }
 
-fn get_expression(tokens: &mut VecDeque<Token>) -> Result<Expression, SyntacticError> {
-    let token = expect_symbol(tokens, "Expression")?;
+struct Parser {
+    tokens: VecDeque<Token>,
+}
 
-    match token {
-        Token::Number(n) => resolve(Expression::Number(n), tokens),
-        Token::SubOp => get_negative_number(tokens),
-        Token::ScopeOpen => get_scope(tokens),
-        _ => Err(SyntacticError::UnexpectedSymbol {
-            before: None,
-            symbol: token,
-        }),
+impl Parser {
+    pub fn new(tokens: VecDeque<Token>) -> Self {
+        Self { tokens }
     }
-}
 
-fn resolve(lhs: Expression, tokens: &mut VecDeque<Token>) -> Result<Expression, SyntacticError> {
-    if tokens.is_empty() {
-        Ok(lhs)
-    } else {
-        get_rhs(lhs, tokens)
+    pub fn get_expression(&mut self) -> Result<Expression, SyntacticError> {
+        let token = self.expect_symbol("Expression")?;
+
+        match token {
+            Token::Number(n) => self.resolve(Expression::Number(n)),
+            Token::SubOp => self.get_negative_number(),
+            Token::ScopeOpen => self.get_scope(),
+            _ => Err(SyntacticError::UnexpectedSymbol {
+                before: None,
+                symbol: token,
+            }),
+        }
     }
-}
 
-fn get_rhs(lhs: Expression, tokens: &mut VecDeque<Token>) -> Result<Expression, SyntacticError> {
-    let token = expect_symbol(tokens, "Operator")?;
-
-    match token {
-        Token::AddOp => get_binary_expression(lhs, tokens, Operator::Add),
-        Token::SubOp => get_binary_expression(lhs, tokens, Operator::Sub),
-        Token::MultOp => get_binary_expression(lhs, tokens, Operator::Mult),
-        Token::DivOp => get_binary_expression(lhs, tokens, Operator::Div),
-        s => Err(SyntacticError::UnexpectedSymbol {
-            before: Some(s),
-            symbol: token,
-        }),
+    fn resolve(&mut self, lhs: Expression) -> Result<Expression, SyntacticError> {
+        if self.tokens.is_empty() {
+            Ok(lhs)
+        } else {
+            self.get_rhs(lhs)
+        }
     }
-}
 
-fn get_binary_expression(
-    lhs: Expression,
-    tokens: &mut VecDeque<Token>,
-    op: Operator,
-) -> Result<Expression, SyntacticError> {
-    let rhs = get_expression(tokens)?;
+    fn get_rhs(&mut self, lhs: Expression) -> Result<Expression, SyntacticError> {
+        let token = self.expect_symbol("Operator")?;
 
-    let exp = Expression::Binary {
-        lhs: Box::new(lhs),
-        rhs: Box::new(rhs),
-        op,
-    };
-
-    Ok(exp)
-}
-
-fn get_scope(tokens: &mut VecDeque<Token>) -> Result<Expression, SyntacticError> {
-    let _token = expect_symbol(tokens, "Expression")?;
-
-    expect_symbol(tokens, "Expression")?;
-
-    todo!()
-}
-
-fn get_negative_number(tokens: &mut VecDeque<Token>) -> Result<Expression, SyntacticError> {
-    let next = expect_symbol(tokens, "Expression")?;
-
-    match next {
-        Token::Number(n) => Ok(Expression::Number(n)),
-        Token::ScopeOpen => get_scope(tokens),
-        _ => todo!(),
+        match token {
+            Token::AddOp => self.get_binary_expression(lhs, Operator::Add),
+            Token::SubOp => self.get_binary_expression(lhs, Operator::Sub),
+            Token::MultOp => self.get_binary_expression(lhs, Operator::Mult),
+            Token::DivOp => self.get_binary_expression(lhs, Operator::Div),
+            s => Err(SyntacticError::UnexpectedSymbol {
+                before: Some(s),
+                symbol: token,
+            }),
+        }
     }
-}
 
-fn expect_symbol(t: &mut VecDeque<Token>, e: &str) -> Result<Token, SyntacticError> {
-    match t.pop_front() {
-        Some(t) => Ok(t),
-        None => Err(SyntacticError::ExpectedSymbol {
-            expected: e.to_owned(),
-        }),
+    fn get_binary_expression(
+        &mut self,
+        lhs: Expression,
+        op: Operator,
+    ) -> Result<Expression, SyntacticError> {
+        let rhs = self.get_expression()?;
+
+        let exp = Expression::Binary {
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+            op,
+        };
+
+        Ok(exp)
+    }
+
+    fn get_scope(&mut self) -> Result<Expression, SyntacticError> {
+        let _token = self.expect_symbol("Expression")?;
+
+        self.expect_symbol("Expression")?;
+
+        todo!()
+    }
+
+    fn get_negative_number(&mut self) -> Result<Expression, SyntacticError> {
+        let next = self.expect_symbol("Expression")?;
+
+        match next {
+            Token::Number(n) => Ok(Expression::Number(n)),
+            Token::ScopeOpen => self.get_scope(),
+            _ => todo!(),
+        }
+    }
+
+    fn expect_symbol(&mut self, e: &str) -> Result<Token, SyntacticError> {
+        match self.tokens.pop_front() {
+            Some(t) => Ok(t),
+            None => Err(SyntacticError::ExpectedSymbol {
+                expected: e.to_owned(),
+            }),
+        }
     }
 }
 
