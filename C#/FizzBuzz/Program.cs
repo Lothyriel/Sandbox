6eba958c22bcd191296ad6d22782de12f4591f0a
João Xavier <fastjonh@gmail.com>
2025-03-19 22:01:31 -0300

    fizz buzz in dotnet
    java sucks https://rohan.ga/blog/java/

---
+++ C#/FizzBuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿var rules = new (Func<int, bool> Rule, Func<int, string> Builder)[]
+{
+    (i => i % 3 == 0, _ => "Fizz"),
+    (i => i % 5 == 0, _ => "Buzz"),
+};
+
+var result = string.Join("\n", FizzBuzz(100));
+
+Console.WriteLine(result);
+
+string[] FizzBuzz(int n)
+{
+    return Enumerable.Range(1, n).Select(BuildNumber).ToArray();
+
+    string BuildNumber(int n)
+    {
+        var result = rules
+            .Where(r => r.Rule(n))
+            .Aggregate("", (acc, rule) => acc += rule.Builder(n));
+
+        return result == "" ? n.ToString() : result;
+    }
+}
